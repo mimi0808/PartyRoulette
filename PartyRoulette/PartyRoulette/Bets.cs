7020d9cbae704e53dc147d4b9098c25785babf29
Michał <mimi0808@boo.pl>
2017-05-13 13:55:26 +0200

    Pierwsze kroki
    Nasze marne początki

---
+++ PartyRoulette/PartyRoulette/Bets.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyRoulette
+{
+    class Bets
+    {
+        List<Bets> ListBets = new List<Bets>();
+    }
+}
---
+++ PartyRoulette/PartyRoulette/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyRoulette
+{
+    class Player
+    {
+        string Name;
+        int Balance;
+
+        
+    }
+}
